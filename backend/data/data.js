a85c47048b19ddb176b2c916b33f66dc2cc627b5
Avik-Man <75834414+Avik-Man@users.noreply.github.com>
2023-03-01 21:11:36 +0530

    Add files via upload

---
+++ backend/data/data.js
@@ -0,0 +1,109 @@
+const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name:" Avik",
+          email: "mandalavik@gmail.com",
+        },
+        {
+          name: "Sayan",
+          email: "sayandas@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName:  "Avik",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Sayan",
+          email: "sayandas@gmail.com",
+        },
+        {
+          name: "Sayani",
+          email: "sayanidas@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468b27c4dd4",
+      chatName: "Sayan",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Sayan",
+          email: "sayandas@gmail.com",
+        },
+        {
+          name: "Soumyadeb",
+          email: "misra@gmail.com",
+        },
+      ],
+      _id: "617a077e18c2d468bc7c4dd4",
+      chatName: "Sayan",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Avik",
+          email: "mandalavik@gmail.com",
+        },
+        {
+          name: "Sayan",
+          email: "sayandas@gmail.com",
+        },
+        {
+          name: "Soumyadeb",
+          email: "misra@gmail.com",
+        },
+      ],
+      _id: "617a518c4081150716472c78",
+      chatName: "CSE A3",
+      groupAdmin: {
+        name: "Avik",
+        email: "guest@example.com",
+      },
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Avik",
+          email: "mandalavik@gmail.com",
+        },
+        {
+          name: "Soumyadeb",
+          email: "misra@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7cfdd4",
+      chatName: "Avik",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Rishav",
+          email: "rishav@gmail.com",
+        },
+        {
+          name: "Avik",
+          email: "mandalavik@gmail.com",
+        },
+        {
+          name: "Sayan",
+          email: "sayandas@gmaile.com",
+        },
+      ],
+      _id: "617a518c4081150016472c78",
+      chatName: "CSE 2024",
+      groupAdmin: {
+        name: "Rishav",
+        email: "rishav@gmail.com",
+      },
+    },
+  ];
+module.exports = {chats};
